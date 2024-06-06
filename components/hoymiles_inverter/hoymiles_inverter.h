97d4fd0c301d937ce92b9b1ce0c2946058364c74
nedyarrd <xperience@interia.pl>
2024-06-06 20:31:57 +0200

    Update hoymiles_inverter.h
    Fix path

--- components/hoymiles_inverter/hoymiles_inverter.h
+++ components/hoymiles_inverter/hoymiles_inverter.h
@@ -4,7 +4,7 @@
 #include "esphome/core/application.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
-#include <Hoymiles.h>
+#include <../../lib/OpenDTU/lib/Hoymiles/src/Hoymiles.h>
 #include <Print.h>
 
 
